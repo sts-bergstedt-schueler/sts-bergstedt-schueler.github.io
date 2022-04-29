5a3ad2196c43aaa74a77e05af760343086055822
sts-bergstedt-schueler <sts-bergstedt-schueler@web.de>
2022-04-29 20:23:57 +0200

    update

---
+++ script.js
@@ -0,0 +1,5 @@
+const textToggle = document.querySelector('.textyou')
+
+            textToggle.addEventListener('click', () => {
+                textToggle.classList.toggle('activ')
+            })
